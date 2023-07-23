cc7455e513f8a7b3d0976c4b0ef62cd85675bac4
salee2 <hleesa>
2023-07-23 16:56:29 +0900

    add example

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -13,7 +13,7 @@ void swap(T& lhs, T& rhs) {
 }
 
 template<typename T>
-void iter(T* array, size_t size, void(* func)(T, T)) {
+void iter(T* array, size_t size, void(* func)(T&, T&)) {
 	for (size_t i = 0; i + 1 < size; ++i) {
 		func(array[i], array[i + 1]);
 	}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -7,9 +7,16 @@ int main()
 {
 	int arr1[] = {1, 2, 3, 4, 5};
 	size_t size1 = sizeof(arr1) / sizeof(arr1[0]);
-	iter(arr1, size1, swap);
+	::iter(arr1, size1, swap);
 	for (size_t i = 0; i < size1; ++i) {
 		std::cout << arr1[i] << " ";
+	}
+	std::cout << "\n======================================\n";
+	double arr2[] = {1.5, 2.5, 3.5, 4.5, 5.5};
+	size_t size2 = sizeof(arr2) / sizeof(arr2[0]);
+	::iter(arr2, size1, swap);
+	for (size_t i = 0; i < size2; ++i) {
+		std::cout << arr2[i] << " ";
 	}
 
 }
