2fc1d07ca1f62918df388a6ee6c38404d29cad20
salee2 <hleesa>
2023-07-23 20:39:23 +0900

    apply one element

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,20 +2,33 @@
 #ifndef CPP_HLEESA_ITER_HPP
 #define CPP_HLEESA_ITER_HPP
 
+#include <iostream>
 #include <stddef.h>
 
 template<typename T>
-void swap(T& lhs, T& rhs) {
-	T temp = lhs;
-	lhs = rhs;
-	rhs = temp;
+void seoul(T& val) {
+	val *= 42;
 	return;
 }
 
 template<typename T>
-void iter(T* array, size_t size, void(* func)(T&, T&)) {
-	for (size_t i = 0; i + 1 < size; ++i) {
-		func(array[i], array[i + 1]);
+void print(const T& val){
+	std::cout << val << '\n';
+	return;
+}
+
+template<typename T>
+void iter(T* array, size_t size, void(* func)(T&)) {
+	for (size_t i = 0; i < size; ++i) {
+		func(array[i]);
+	}
+	return ;
+}
+
+template<typename T>
+void iter(const T* array, size_t size, void(* func)(const T&)) {
+	for (size_t i = 0; i < size; ++i) {
+		func(array[i]);
 	}
 	return ;
 }
