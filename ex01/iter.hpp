e176ba208faa3e0e4846e5eb51c711d5a5800191
salee2 <hleesa>
2023-07-25 15:54:54 +0900

    apply return type T

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,31 +6,31 @@
 #include <stddef.h>
 
 template<typename T>
-void seoul(T& val) {
+T seoul(T& val) {
 	val *= 42;
-	return;
+	return val;
 }
 
 template<typename T>
-void print(const T& val){
+T print(const T& val) {
 	std::cout << val << '\n';
+	return val;
+}
+
+template<typename T>
+void iter(T* array, size_t size, T(* func)(T&)) {
+	for (size_t i = 0; i < size; ++i) {
+		func(array[i]);
+	}
 	return;
 }
 
 template<typename T>
-void iter(T* array, size_t size, void(* func)(T&)) {
+void iter(const T* array, size_t size, T(* func)(const T&)) {
 	for (size_t i = 0; i < size; ++i) {
 		func(array[i]);
 	}
-	return ;
-}
-
-template<typename T>
-void iter(const T* array, size_t size, void(* func)(const T&)) {
-	for (size_t i = 0; i < size; ++i) {
-		func(array[i]);
-	}
-	return ;
+	return;
 }
 
 
